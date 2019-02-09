ed131d3ba5c171d2ae59252954738bc2cb37b413
Uros Trifunovic <uros.trifunovic@adidas.com>
2019-02-09 11:30:17 +0100

    Roman numerals kata WIP

--- roman-numerals/src/main/java/com/urosht/demo/RomanNumerals.java
+++ roman-numerals/src/main/java/com/urosht/demo/RomanNumerals.java
@@ -1,5 +1,37 @@
 package com.urosht.demo;
 
+import java.util.HashMap;
+
 public class RomanNumerals {
 
+
+    private static HashMap<Integer, String> numeralsMap = new HashMap<Integer, String>() {{
+        put(1, "I");
+        put(5, "V");
+        put(10, "X");
+    }};
+
+    public static String transform(int arabic) {
+        String roman = null;
+        if (arabic > 1 && arabic < 4) {
+            roman = transform(1) + transform(arabic - 1);
+        } else if (mustSubtractOne(arabic)) {
+            roman = transform(1) + transform(arabic + 1);
+        } else if (arabic > 5 && arabic < 9) {
+            roman = transform(5) + transform(arabic - 5);
+        } else if (arabic > 10 && arabic < 14) {
+            roman = transform(10) + transform(arabic - 10);
+        } else {
+            roman = numeralsMap.get(arabic);
+        }
+        return roman;
+    }
+
+    private static boolean mustSubtractOne(int x) {
+        return x % 5 == 4;
+    }
+
+    private static boolean mustAddOne(int x) {
+        return false;
+    }
 }
--- roman-numerals/src/test/java/com/urosht/demo/RomanNumeralsTest.java
+++ roman-numerals/src/test/java/com/urosht/demo/RomanNumeralsTest.java
@@ -1,11 +1,37 @@
 package com.urosht.demo;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class RomanNumeralsTest {
 
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {1, "I"}, {2, "II"}, {3, "III"}, {4, "IV"},
+                {5, "V"}, {6, "VI"}, {7, "VII"}, {8, "VIII"},
+                {9, "IX"}, {10, "X"}, {11, "XI"}, {12, "XII"},
+                {13, "XIII"}, {14, "XIV"}
+        });
+    }
 
+    private int arabic;
+    private String roman;
 
+    public RomanNumeralsTest(int arabic, String roman) {
+        this.arabic = arabic;
+        this.roman = roman;
+    }
+
+    @Test
+    public void givenArabicNumber_whenParsing_thenReturnRoman() {
+        Assert.assertEquals(roman, RomanNumerals.transform(arabic));
+    }
 }
